8c0acbfde28a38072090b05641a61c1fe7292ef8
viktormashyka <99292051+viktormashyka@users.noreply.github.com>
2025-02-07 11:29:14 +0000

    add warning

--- library.py
+++ library.py
@@ -1,24 +1,30 @@
+class Book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
 class Library:
     def __init__(self):
         self.books = []
 
     def add_book(self, title, author, year):
-        book = {
-            "title": title,
-            "author": author,
-            "year": year
-        }
-        self.books.append(book)
+        self.books.append(Book(title, author, year))
 
     def remove_book(self, title):
         for book in self.books:
-            if book["title"] == title:
+            if book.title == title:
                 self.books.remove(book)
                 break
+            else:
+                print(f'Book with title {title} not found in library.')
 
     def show_books(self):
+        if not self.books:
+            print("No books in library.")
+            return
         for book in self.books:
-            print(f'Title: {book["title"]}, Author: {book["author"]}, Year: {book["year"]}')
+            print(f'Title: {book.title}, Author: {book.author}, Year: {book.year}')
 
 def main():
     library = Library()
